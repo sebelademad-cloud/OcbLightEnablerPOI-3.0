9257924f4f4dd23b4eafa2bd4c2ae92be866f89f
Sebastian <Sebastian@DESKTOP-SOE2OTQ>
2026-07-11 17:52:58 +0200

    Add project files.

---
+++ OcbLightEnablerPOI/Harmony/ModApi.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+
+namespace OcbLightEnablerPOI
+{
+    public class OcbLightEnablerPOI : IModApi
+    {
+        public void InitMod(Mod mod)
+        {
+            new Harmony("ocb.lightenablerpoi").PatchAll();
+        }
+    }
+}
